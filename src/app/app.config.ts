147d9a9fb44373ce155495d09669ce4b7396f21c
Samir Vergara V <svergara3@misena.edu.co>
2025-10-26 14:02:41 -0500

    Update environments + route /#/

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,15 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    {
+      provide: LocationStrategy,
+      useClass: HashLocationStrategy,
+    },
+  ],
 };
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+
+  export const environment = {
+    production: false,
+    mapboxKey:"pk.eyJ1Ijoic3ZlcmdhcmEiLCJhIjoiY2tzYjQ4NzNlMDM2MzJvbnY1YTg2dXQwZyJ9.lOWA31Gavf545Qd_RKrcLw"
+  };
